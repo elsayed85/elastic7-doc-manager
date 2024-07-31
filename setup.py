b21c014695e4d9ef9870d1a896f99926255ee8a6
Elsayed Kamal <elsayedkamal581999@gmail.com>
2024-07-31 13:39:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setuptools.setup(
     platforms=["any"],
     author="anna herlihy",
     author_email="mongodb-user@googlegroups.com, tharun@nineleaps.com",
-    url="https://github.com/tmpaul06/elastic7-doc-manager",
+    url="https://github.com/elsayed85/elastic7-doc-manager",
     install_requires=["mongo-connector>=2.5.0", "importlib_metadata"],
     python_requires=">=3.4",
     extras_require={
         "aws": ["boto3 >= 1.4.0", "requests-aws-sign >= 0.1.2"],
-        "elastic7": ["elasticsearch>=7.0.0<8.0.0"]
+        "elastic7": ["elasticsearch>=7.0.0"]
     },
     packages=["mongo_connector", "mongo_connector.doc_managers"],
     classifiers=[
